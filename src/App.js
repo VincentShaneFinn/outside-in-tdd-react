cbf67eddd314c954ba39e9da3a61e5ab86a02c10
Vincent Finn <vince.finn@focusedlabs.io>
2022-06-12 12:02:47 -0500

    added restaurant screen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import {RestaurantScreen} from './components/RestaurantScreen';
+
 export default function App() {
-  return <div>Hello, world.</div>;
+  return (<div>
+    <RestaurantScreen />
+  </div>);
 }
---
+++ src/components/RestaurantScreen.js
@@ -0,0 +1,14 @@
+export function RestaurantScreen() {
+  return (
+    <div>
+      <h1>Restaurants</h1>
+      <RestaurantList />
+    </div>
+  )
+}
+
+function RestaurantList() {
+  return (<div>
+    Restaurant List
+  </div>)
+}
